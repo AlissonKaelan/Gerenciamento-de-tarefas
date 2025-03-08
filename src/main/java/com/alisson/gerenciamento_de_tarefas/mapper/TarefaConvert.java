8daa9a50a882838246febbc5ddada5e3d1f63330
AlissonKaelan <alissonkaelanc@gmail.com>
2025-03-08 20:52:30 -0300

    Add TarefaDto and test TarefasService

---
+++ src/main/java/com/alisson/gerenciamento_de_tarefas/mapper/TarefaConvert.java
@@ -0,0 +1,34 @@
+package com.alisson.gerenciamento_de_tarefas.mapper;
+
+import com.alisson.gerenciamento_de_tarefas.api.TarefaDto;
+import com.alisson.gerenciamento_de_tarefas.database.entity.TarefaEntity;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class TarefaConvert {
+    public TarefaEntity convertTarefaEntity(final TarefaDto tarefaDto){
+            TarefaEntity tarefaEntity = new TarefaEntity();
+            tarefaEntity.setTitle(tarefaDto.getTitle());
+            tarefaEntity.setStatus(tarefaDto.getStatus());
+            tarefaEntity.setDescription(tarefaDto.getDescription());
+            tarefaEntity.setUpdatedOn(tarefaDto.getUpdatedOn());
+            tarefaEntity.setExpireOn(tarefaDto.getExpireOn());
+            tarefaEntity.setCreatedOn(tarefaDto.getCreatedOn());
+            return tarefaEntity;
+    }
+
+    public TarefaDto convertTarefaDto(final TarefaEntity tarefaEntity){
+            TarefaDto tarefaDto = new TarefaDto();
+            tarefaDto.setId(tarefaDto.getId());
+            tarefaDto.setTitle(tarefaDto.getTitle());
+            tarefaDto.setPriority(tarefaDto.getPriority());
+            tarefaDto.setDescription(tarefaDto.getDescription());
+            tarefaDto.setStatus(tarefaDto.getStatus());
+            tarefaDto.setUpdatedOn(tarefaDto.getUpdatedOn());
+            tarefaDto.setExpireOn(tarefaDto.getExpireOn());
+            tarefaDto.setCreatedOn(tarefaDto.getCreatedOn());
+            return tarefaDto;
+
+    }
+}
--- src/main/java/com/alisson/gerenciamento_de_tarefas/service/TarefasService.java
+++ src/main/java/com/alisson/gerenciamento_de_tarefas/service/TarefasService.java
@@ -1,43 +1,52 @@
 package com.alisson.gerenciamento_de_tarefas.service;
 import com.alisson.gerenciamento_de_tarefas.api.Priority;
 import com.alisson.gerenciamento_de_tarefas.api.Status;
+import com.alisson.gerenciamento_de_tarefas.api.TarefaDto;
 import com.alisson.gerenciamento_de_tarefas.database.entity.TarefaEntity;
 import com.alisson.gerenciamento_de_tarefas.database.repository.TarefaRepository;
+import com.alisson.gerenciamento_de_tarefas.mapper.TarefaConvert;
 import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
 
-import java.time.Instant;
 
 import static java.time.Instant.*;
-
+@Service
 public class TarefasService {
     private final TarefaRepository tarefaRepository;
 
+    private final TarefaConvert tarefaConvert;
+
     //Constructor
-    public TarefasService(final TarefaRepository tarefaRepository){
+    public TarefasService(final TarefaRepository tarefaRepository, final TarefaConvert tarefaConvert){
         this.tarefaRepository = tarefaRepository;
+        this.tarefaConvert = tarefaConvert;
     }
 
     @PostConstruct
     private void generateRandomTask(){
-        System.out.println("Iniciando a geração de uma tarefa aleatória...");
-        TarefaEntity tarefaEntity = new TarefaEntity();
 
-        tarefaEntity.setTitle("Aulas de Programacao");
-        tarefaEntity.setDescription("Hello World " + "Calculadora Simples " + "Contador de Palavras " + "Jogo da Adivinhação " + "Lista de Tarefas " + "Conversor de Unidades");
-        tarefaEntity.setStatus(Status.Ready);
-        tarefaEntity.setPriority(Priority.High);
-        tarefaEntity.setUpdatedOn(now());
-        tarefaEntity.setExpireOn(now());
-        tarefaEntity.setCreatedOn(now());
+        TarefaDto tarefaDto = new TarefaDto();
+        tarefaDto.setTitle("Aulas de Piano");
+        tarefaDto.setDescription("Aula dia 24 " + "Professor Jubuleu" + "14hrs" + "Praca 4");
+        tarefaDto.setStatus(Status.Progress);
+        tarefaDto.setPriority(Priority.Low);
+        tarefaDto.setUpdatedOn(now());
+        tarefaDto.setExpireOn(now());
+        tarefaDto.setCreatedOn(now());
 
-        saveTarefa(tarefaEntity);
+        saveTarefa(tarefaDto);
 
 
     }
 
 
-    public void saveTarefa(final  TarefaEntity tarefaEntity){
-        tarefaRepository.save(tarefaEntity);
+    public void saveTarefa(final TarefaDto tarefaDto){
+        try{
+            final TarefaEntity tarefaEntity = tarefaConvert.convertTarefaEntity(tarefaDto);
+            tarefaRepository.save(tarefaEntity);
+        } catch (RuntimeException re) {
+            throw re;
+        }
 
     }
 }
