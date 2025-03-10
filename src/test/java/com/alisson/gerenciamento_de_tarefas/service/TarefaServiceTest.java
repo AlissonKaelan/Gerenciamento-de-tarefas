e68c353277ba2012b0914580f69d84cc144696a8
AlissonKaelan <alissonkaelanc@gmail.com>
2025-03-10 17:11:14 -0300

    Added new unit tests to the TaskServiceTest class for getTaskById, deleteTask, updateTask and getTaskList.

--- src/test/java/com/alisson/gerenciamento_de_tarefas/service/TarefaServiceTest.java
+++ src/test/java/com/alisson/gerenciamento_de_tarefas/service/TarefaServiceTest.java
@@ -1,5 +1,7 @@
 package com.alisson.gerenciamento_de_tarefas.service;
 
+import com.alisson.gerenciamento_de_tarefas.api.Priority;
+import com.alisson.gerenciamento_de_tarefas.api.Status;
 import com.alisson.gerenciamento_de_tarefas.api.TarefaDto;
 import com.alisson.gerenciamento_de_tarefas.database.entity.TarefaEntity;
 import com.alisson.gerenciamento_de_tarefas.database.repository.TarefaRepository;
@@ -10,6 +12,14 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 public class TarefaServiceTest {
@@ -36,4 +46,77 @@ public class TarefaServiceTest {
         verify(tarefaRepository, times(1)).save(tarefaEntity);
 
     }
+
+    @Test
+    public void testGetTarefaById(){
+        UUID id = UUID.randomUUID();
+        TarefaEntity tarefaEntity = new TarefaEntity();
+        tarefaEntity.setId(id);
+
+        when(tarefaRepository.findById(id)).thenReturn(Optional.of(tarefaEntity));
+
+        TarefaDto tarefaDto = new TarefaDto();
+        when(tarefaConvert.convertTarefaEntityTOTarefaDto(tarefaEntity)).thenReturn(tarefaDto);
+
+        TarefaDto tarefaDtoResult = tarefasService.getTarefaById(id);
+
+        assertNotNull(tarefaDtoResult);
+        assertEquals(tarefaDto, tarefaDtoResult);
+
+        verify(tarefaRepository, times(1)).findById(id);
+        verify(tarefaConvert,times(1)).convertTarefaEntityTOTarefaDto(tarefaEntity);
+    }
+
+    @Test
+    public void testDeleteTarefa(){
+        UUID id = UUID.randomUUID();
+        tarefasService.deletTarefa(id);
+        verify(tarefaRepository,times(1)).deleteById(id);
+    }
+
+    @Test
+    public void testUpdateTarefa(){
+        TarefaDto tarefaDto = new TarefaDto();
+        tarefaDto.setId(UUID.randomUUID());
+        tarefaDto.setTitle("Titulo update");
+        Instant date = Instant.now();
+        tarefaDto.setStatus(Status.Progress);
+        tarefaDto.setDescription("Update description");
+        tarefaDto.setPriority(Priority.High);
+
+        TarefaEntity tarefaEntity = new TarefaEntity();
+        tarefaEntity.setId(tarefaDto.getId());
+        when(tarefaRepository.findById(tarefaDto.getId())).thenReturn(Optional.of(tarefaEntity));
+
+        tarefasService.updateTarefa(tarefaDto);
+
+        verify(tarefaRepository,times(1)).findById(tarefaDto.getId());
+        verify(tarefaRepository,times(1)).save(tarefaEntity);
+
+        assertEquals("Titulo update", tarefaEntity.getTitle());
+        assertEquals("Update description", tarefaEntity.getDescription());
+        assertEquals(Status.Progress, tarefaEntity.getStatus());
+        assertEquals(Priority.High, tarefaEntity.getPriority());
+
+    }
+
+    @Test
+    public void testGetTarefaList(){
+        List<TarefaEntity> tarefaEntityList = Arrays.asList(new TarefaEntity(), new TarefaEntity(), new TarefaEntity());
+        when(tarefaRepository.findAllByOrderByCreatedOnDesc()).thenReturn(tarefaEntityList);
+
+        TarefaDto tarefaDto = new TarefaDto();
+        TarefaEntity tarefaEntity = new TarefaEntity();
+        when(tarefaConvert.convertTarefaEntityTOTarefaDto(tarefaEntity)).thenReturn(tarefaDto);
+
+        List<TarefaDto> tarefaDtoList = tarefasService.getTarefaList();
+
+        assertNotNull(tarefaDtoList);
+        assertEquals(3, tarefaDtoList.size());
+        verify(tarefaRepository, times(1)).findAllByOrderByCreatedOnDesc();
+        verify(tarefaConvert, times(3)).convertTarefaEntityTOTarefaDto((TarefaEntity) any());
+
+
+    }
+
 }
